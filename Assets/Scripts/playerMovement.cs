8ec5187231c6ee0afc2d09eb47213eeb9a7f2041
Ghh0ost <530416@escg.ac.uk>
2022-10-19 19:18:50 +0100

    v1

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        
+    }
+}
